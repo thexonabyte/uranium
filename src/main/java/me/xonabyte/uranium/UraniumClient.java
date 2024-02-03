9b6ec149ed02ad4cf58d1b17f1e9543370c5c930
LordFoxter <foxter144@yahoo.com>
2024-02-03 12:03:54 +0100

    Initial commit

---
+++ src/main/java/me/xonabyte/uranium/UraniumClient.java
@@ -0,0 +1,10 @@
+package me.xonabyte.uranium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UraniumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
